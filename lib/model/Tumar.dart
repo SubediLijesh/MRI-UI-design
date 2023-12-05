c6a4e0dbfee84495d67cb26ff8603d9a88afe9bc
lijesh subedi <subedilijesh.com>
2023-12-05 20:54:08 +0545

    before model integration

--- lib/model/Tumar.dart
+++ lib/model/Tumar.dart
@@ -14,71 +14,71 @@ List<Tumor> tumors = [
   Tumor(
       tumorName: '1. Neurocytoma',
       tumorDes:
-          'Central neurocytomas are generally noncancerous (benign) brain tumors in the ventricles, which are the fluid-filled spaces within the brain',
+          '-Central neurocytomas are generally noncancerous (benign) brain tumors in the ventricles, which are the fluid-filled spaces within the brain',
       imageUrl: 'images/Neurocytoma.jpg'),
   Tumor(
       tumorName: '2. Tuberculoma',
       tumorDes:
-          'Tuberculomas or tuberculous granulomas are well defined focal masses that result from Mycobacterium tuberculosis infection and are one of the more severe morphological forms of tuberculosis',
-      imageUrl: 'ggggggg'),
+          '-Tuberculomas or tuberculous granulomas are well defined focal masses that result from Mycobacterium tuberculosis infection and are one of the more severe morphological forms of tuberculosis',
+      imageUrl: 'images/Tuberculoma.png'),
   Tumor(
       tumorName: '3. Schwannoma',
       tumorDes:
-          'Schwannoma is a rare type of tumor that forms in the nervous system. Schwannoma grows from cells called Schwann cells. Schwann cells protect and support the nerve cells of the nervous system. Schwannoma tumors are often benign, which means they are not cancer. But, in rare cases, they can become cancer',
-      imageUrl: 'ggggggg'),
+          '-Schwannoma is a rare type of tumor that forms in the nervous system. Schwannoma grows from cells called Schwann cells. Schwann cells protect and support the nerve cells of the nervous system. Schwannoma tumors are often benign, which means they are not cancer. But, in rare cases, they can become cancer',
+      imageUrl: 'images/Schwannoma.jpg'),
   Tumor(
       tumorName: '4. Papilloma',
       tumorDes:
-          'A papilloma (plural papillomas or papillomata) (papillo- + -oma) is a benign epithelial tumor growing exophytically (outwardly projecting) in nipple-like and often finger-like fronds. In this context, papilla refers to the projection created by the tumor, not a tumor on an already existing papilla (such as the nipple',
-      imageUrl: 'ggggggg'),
+          '-A papilloma (plural papillomas or papillomata) (papillo- + -oma) is a benign epithelial tumor growing exophytically (outwardly projecting) in nipple-like and often finger-like fronds. In this context, papilla refers to the projection created by the tumor, not a tumor on an already existing papilla (such as the nipple',
+      imageUrl: 'images/Papilloma.jpeg'),
   Tumor(
       tumorName: '5. Meningioma',
       tumorDes:
-          'Meningioma is the most common type of primary brain tumor, accounting for approximately 30 percent of all brain tumors',
-      imageUrl: 'ggggggg'),
+          '-Meningioma is the most common type of primary brain tumor, accounting for approximately 30 percent of all brain tumors',
+      imageUrl: 'images/Meningioma.jpg'),
   Tumor(
       tumorName: '6. Oligodendroglioma',
       tumorDes:
-          'Oligodendroglioma is a growth of cells that starts in the brain.These cells make a substance that protects nerve cells and helps with the flow of electrical signals in the brain and spinal cord',
-      imageUrl: 'gggggg'),
+          '-Oligodendroglioma is a growth of cells that starts in the brain.These cells make a substance that protects nerve cells and helps with the flow of electrical signals in the brain and spinal cord',
+      imageUrl: 'images/Oligodendroglioma.jpg'),
   Tumor(
       tumorName: '7. Meduloblastoma',
       tumorDes:
-          'Medulloblastoma is a primary central nervous system (CNS) tumor. This means it begins in the brain or spinal cord',
-      imageUrl: 'gggggg'),
+          '-Medulloblastoma is a primary central nervous system (CNS) tumor. This means it begins in the brain or spinal cord',
+      imageUrl: 'images/Meduloblastoma.jpg'),
   Tumor(
       tumorName: '8. Granuloma',
       tumorDes:
-          'A granuloma is a tiny cluster of white blood cells and other tissue. It can appear in your lungs, skin or other parts of your body',
-      imageUrl: 'gggggggg'),
+          '-A granuloma is a tiny cluster of white blood cells and other tissue. It can appear in your lungs, skin or other parts of your body',
+      imageUrl: 'images/Granuloma.jfif'),
   Tumor(
       tumorName: '9. Glioblastoma',
       tumorDes:
-          'Glioblastoma is the most common primary brain cancer, or cancer that starts in the brain, with around 12,000 cases diagnosed in the United States each year',
-      imageUrl: 'ggggggg'),
+          '-Glioblastoma is the most common primary brain cancer, or cancer that starts in the brain, with around 12,000 cases diagnosed in the United States each year',
+      imageUrl: 'images/Glioblastoma.jpg'),
   Tumor(
       tumorName: '10. Germinoma',
       tumorDes:
-          'Germinomas are rare tumors that primarily occur in children and adolescents. These tumors arise from germ cells, which normally become eggs or sperm after migrating to the ovaries or testes during fetal development',
-      imageUrl: 'ggggggg'),
+          '-Germinomas are rare tumors that primarily occur in children and adolescents. These tumors arise from germ cells, which normally become eggs or sperm after migrating to the ovaries or testes during fetal development',
+      imageUrl: 'images/Germinoma.jpg'),
   Tumor(
       tumorName: '11. Ependimoma',
       tumorDes:
-          'An ependymoma is a primary central nervous system (CNS) tumor. This means it begins in the brain or spinal cord. To get an accurate diagnosis, a piece of tumor tissue will be removed during surgery, if possible',
-      imageUrl: 'ggggggg'),
+          '-An ependymoma is a primary central nervous system (CNS) tumor. This means it begins in the brain or spinal cord. To get an accurate diagnosis, a piece of tumor tissue will be removed during surgery, if possible',
+      imageUrl: 'images/Ependimoma.jpg'),
   Tumor(
       tumorName: '12. Ganglioglioma',
       tumorDes:
-          'A ganglioglioma is low-grade tumor of mixed cell type. It is very rare and contains properties of both glial cells — responsible for providing the structural support of the central nervous system, and neuronal cells — the functioning component of the central nervous system',
-      imageUrl: 'gggggg'),
+          '-A ganglioglioma is low-grade tumor of mixed cell type. It is very rare and contains properties of both glial cells — responsible for providing the structural support of the central nervous system, and neuronal cells — the functioning component of the central nervous system',
+      imageUrl: 'images/Ganglioglioma.jfif'),
   Tumor(
       tumorName: '13. Carcinoma',
       tumorDes:
-          'Specifically, a carcinoma is a cancer that begins in a tissue that lines the inner or outer surfaces of the body, and that arises from cells originating in the endodermal, mesodermal or ectodermal germ layer during embryogenesis. Carcinoma',
-      imageUrl: 'gggggg'),
+          '-Specifically, a carcinoma is a cancer that begins in a tissue that lines the inner or outer surfaces of the body, and that arises from cells originating in the endodermal, mesodermal or ectodermal germ layer during embryogenesis. Carcinoma',
+      imageUrl: 'images/Carcinoma.jpg'),
   Tumor(
       tumorName: '14. Astrocitoma',
       tumorDes:
-          'Astrocytoma is a type of cancer that can occur in the brain or spinal cord. It begins in cells called astrocytes that support nerve cells',
-      imageUrl: 'ggggggg'),
+          '-Astrocytoma is a type of cancer that can occur in the brain or spinal cord. It begins in cells called astrocytes that support nerve cells',
+      imageUrl: 'images/Astrocitoma.jfif'),
 ];
